0769348022d1eba26e1c39146df32dc82e5a7b0e
Peeter <a72084@ut.ee>
2010-11-26 16:05:05 +0200

    Working on GameController and GUI

--- Mancala/src/sysmod/mancala/GameController.java
+++ Mancala/src/sysmod/mancala/GameController.java
@@ -5,90 +5,114 @@ import java.beans.PropertyChangeListener;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.sound.midi.ControllerEventListener;
 import javax.swing.SwingUtilities;
 
 public class GameController {
-	
-	//model objects
+
+	// model objects
 	private List<AbstractPit> pits = new ArrayList<AbstractPit>();
 	private Player playerOne;
 	private Player playerTwo;
-	
+
 	private PropertyChangeListener seedsChangeListener = new PropertyChangeListener() {
 
 		@Override
 		public void propertyChange(PropertyChangeEvent evt) {
-			
-			
+
 		}
-		
+
 	};
-	
+
 	private PropertyChangeListener turnChangeListener = new PropertyChangeListener() {
 
 		@Override
 		public void propertyChange(PropertyChangeEvent evt) {
-			
-			
+
 		}
-		
+
 	};
-	
-	private void initializePits(){
+
+	private void initializePits() {
 		AbstractPit pit = new Pit();
+		pit.setSeeds(4);
 		pit.setPlayer(playerOne);
 		pits.add(pit);
-		
-		pit.addPropertyChangeListener(AbstractPit.PROPERTY_SEEDS, seedsChangeListener);
-		
+
+		pit.addPropertyChangeListener(AbstractPit.PROPERTY_SEEDS,
+				seedsChangeListener);
+
 		AbstractPit newPit;
-		
-		for(int i = 2; i<=14; i++){
-			if(i==7 || i==14)
+
+		for (int i = 2; i <= 14; i++) {
+			if (i == 7 || i == 14)
 				newPit = new Store();
-			else
+			else {
 				newPit = new Pit();
-			
-			if(i<=7)
+				newPit.setSeeds(4);
+			}
+
+			if (i <= 7)
 				newPit.setPlayer(playerOne);
 			else
 				newPit.setPlayer(playerTwo);
-			
+
 			pit.setNextPit(newPit);
 			pits.add(newPit);
 			pit = newPit;
-			if(i==14)
+			if (i == 14)
 				newPit.setNextPit(pits.get(0));
-			
-			//add property change listeners
-			pit.addPropertyChangeListener(AbstractPit.PROPERTY_SEEDS, seedsChangeListener);
+
+			// add property change listeners
+			pit.addPropertyChangeListener(AbstractPit.PROPERTY_SEEDS,
+					seedsChangeListener);
 		}
-		
-		Pit  opit;
-		for(int i=0; i<6;i++){
+
+		Pit opit;
+		for (int i = 0; i < 6; i++) {
 			opit = (Pit) pits.get(i);
-			opit.setOppositePit((Pit) pits.get(12-i));
+			opit.setOppositePit((Pit) pits.get(12 - i));
 		}
 	}
-	
-	public static void main(String[] args){
-		
+
+	private void updateGUI(MancalaGUI gui) {
+
+		gui.getPit1().setText(Integer.toString(this.pits.get(0).getSeeds()));
+		gui.getPit2().setText(Integer.toString(this.pits.get(1).getSeeds()));
+		gui.getPit3().setText(Integer.toString(this.pits.get(2).getSeeds()));
+		gui.getPit4().setText(Integer.toString(this.pits.get(3).getSeeds()));
+		gui.getPit5().setText(Integer.toString(this.pits.get(4).getSeeds()));
+		gui.getPit6().setText(Integer.toString(this.pits.get(5).getSeeds()));
+		gui.getPit7().setText(Integer.toString(this.pits.get(7).getSeeds()));
+		gui.getPit8().setText(Integer.toString(this.pits.get(8).getSeeds()));
+		gui.getPit9().setText(Integer.toString(this.pits.get(9).getSeeds()));
+		gui.getPit10().setText(Integer.toString(this.pits.get(10).getSeeds()));
+		gui.getPit11().setText(Integer.toString(this.pits.get(11).getSeeds()));
+		gui.getPit12().setText(Integer.toString(this.pits.get(12).getSeeds()));
+
+	}
+
+	public static void main(String[] args) {
+
 		// sets up the gameboard
-		GameController controller = new GameController();
+		final GameController controller = new GameController();
 		controller.initializePits();
-		//TODO bind gameboard status with gui text
 		// launches the gui
+		final MancalaGUI gui = new MancalaGUI();
+		controller.updateGUI(gui);
 		SwingUtilities.invokeLater(new Runnable() {
 			public void run() {
-				MancalaGUI gui = new MancalaGUI();
 				gui.getJFrame().setVisible(true);
 			}
 		});
-		
-		/*controller.pits.get(10).setSeeds(221);
-		controller.pits.get(2).setSeeds(666);
-		System.out.println(controller.pits.size());
-		System.out.println(controller.pits.get(13).getClass());
-		System.out.println(((Pit)controller.pits.get(10)).getOppositePit().getSeeds());*/
+
+		/*
+		 * controller.pits.get(10).setSeeds(221);
+		 * controller.pits.get(2).setSeeds(666);
+		 * System.out.println(controller.pits.size());
+		 * System.out.println(controller.pits.get(13).getClass());
+		 * System.out.println
+		 * (((Pit)controller.pits.get(10)).getOppositePit().getSeeds());
+		 */
 	}
 }
--- Mancala/src/sysmod/mancala/MancalaGUI.java
+++ Mancala/src/sysmod/mancala/MancalaGUI.java
@@ -1,19 +1,10 @@
 package sysmod.mancala;
 
-import java.awt.event.KeyEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
-import java.awt.Event;
 import java.awt.BorderLayout;
 import javax.swing.SwingConstants;
-import javax.swing.SwingUtilities;
-import javax.swing.KeyStroke;
 import java.awt.Point;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JMenuItem;
-import javax.swing.JMenuBar;
-import javax.swing.JMenu;
 import javax.swing.JFrame;
 import javax.swing.JDialog;
 import java.awt.Dimension;
@@ -24,7 +15,6 @@ import java.awt.GridBagConstraints;
 import java.awt.Font;
 import java.awt.Rectangle;
 import java.awt.SystemColor;
-import java.awt.Color;
 import javax.swing.BorderFactory;
 import javax.swing.JTextPane;
 
@@ -32,7 +22,7 @@ public class MancalaGUI {
 
 	private JFrame jFrame = null;
 	private JPanel jContentPane = null;
-	private JDialog teamDialog = null;  //  @jve:decl-index=0:visual-constraint="19,332"
+	private JDialog teamDialog = null; // @jve:decl-index=0:visual-constraint="19,332"
 	private JPanel aboutContentPane = null;
 	private JTextArea aboutText = null;
 	private JLabel aboutTitle = null;
@@ -57,11 +47,12 @@ public class MancalaGUI {
 	private JPanel rightPanel = null;
 	private JButton helpButton = null;
 	private JButton teamButton = null;
-	private JDialog helpDialog = null;  //  @jve:decl-index=0:visual-constraint="336,331"
+	private JDialog helpDialog = null; // @jve:decl-index=0:visual-constraint="336,331"
 	private JPanel help = null;
 	private JLabel helpTitle = null;
 	private JTextPane helpText = null;
 	private JButton newgameButton = null;
+
 	/**
 	 * This method initializes jFrame
 	 * 
@@ -101,8 +92,8 @@ public class MancalaGUI {
 	}
 
 	/**
-	 * This method initializes teamDialog	
-	 * 	
+	 * This method initializes teamDialog
+	 * 
 	 * @return javax.swing.JDialog
 	 */
 	private JDialog getTeamDialog() {
@@ -137,9 +128,9 @@ public class MancalaGUI {
 	}
 
 	/**
-	 * This method initializes aboutText	
-	 * 	
-	 * @return javax.swing.JTextArea	
+	 * This method initializes aboutText
+	 * 
+	 * @return javax.swing.JTextArea
 	 */
 	private JTextArea getAboutText() {
 		if (aboutText == null) {
@@ -147,15 +138,16 @@ public class MancalaGUI {
 			aboutText.setSize(100, 70);
 			aboutText.setName("About");
 			aboutText.setBackground(SystemColor.activeCaptionBorder);
-			aboutText.setText(" - Martin Loginov \n - Hans M�esalu \n - Peeter J�rviste \n - Mari R��tli \n - Sven Aller");
+			aboutText
+					.setText(" - Martin Loginov \n - Hans M�esalu \n - Peeter J�rviste \n - Mari R��tli \n - Sven Aller");
 		}
 		return aboutText;
 	}
 
 	/**
-	 * This method initializes Gameboard	
-	 * 	
-	 * @return javax.swing.JPanel	
+	 * This method initializes Gameboard
+	 * 
+	 * @return javax.swing.JPanel
 	 */
 	private JPanel getGameboard() {
 		if (Gameboard == null) {
@@ -253,19 +245,20 @@ public class MancalaGUI {
 	}
 
 	/**
-	 * This method initializes pit12	
-	 * 	
-	 * @return javax.swing.JButton	
+	 * This method initializes pit12
+	 * 
+	 * @return javax.swing.JButton
 	 */
-	private JButton getPit12() {
+	JButton getPit12() {
 		if (pit12 == null) {
 			pit12 = new JButton();
-			pit12.setText("3");
 			pit12.setFont(new Font("Dialog", Font.BOLD, 14));
 			pit12.setPreferredSize(new Dimension(60, 26));
 			pit12.addMouseListener(new java.awt.event.MouseAdapter() {
 				public void mouseClicked(java.awt.event.MouseEvent e) {
-					System.out.println("mouseClicked()"); // TODO Auto-generated Event stub mouseClicked()
+					System.out.println("mouseClicked()"); // TODO Auto-generated
+					// Event stub
+					// mouseClicked()
 				}
 			});
 		}
@@ -273,19 +266,20 @@ public class MancalaGUI {
 	}
 
 	/**
-	 * This method initializes pit11	
-	 * 	
-	 * @return javax.swing.JButton	
+	 * This method initializes pit11
+	 * 
+	 * @return javax.swing.JButton
 	 */
-	private JButton getPit11() {
+	JButton getPit11() {
 		if (pit11 == null) {
 			pit11 = new JButton();
-			pit11.setText("3");
 			pit11.setFont(new Font("Dialog", Font.BOLD, 14));
 			pit11.setPreferredSize(new Dimension(60, 26));
 			pit11.addMouseListener(new java.awt.event.MouseAdapter() {
 				public void mouseClicked(java.awt.event.MouseEvent e) {
-					System.out.println("mouseClicked()"); // TODO Auto-generated Event stub mouseClicked()
+					System.out.println("mouseClicked()"); // TODO Auto-generated
+					// Event stub
+					// mouseClicked()
 				}
 			});
 		}
@@ -293,19 +287,20 @@ public class MancalaGUI {
 	}
 
 	/**
-	 * This method initializes pit1	
-	 * 	
-	 * @return javax.swing.JButton	
+	 * This method initializes pit1
+	 * 
+	 * @return javax.swing.JButton
 	 */
-	private JButton getPit1() {
+	JButton getPit1() {
 		if (pit1 == null) {
 			pit1 = new JButton();
-			pit1.setText("3");
 			pit1.setFont(new Font("Dialog", Font.BOLD, 14));
 			pit1.setPreferredSize(new Dimension(60, 26));
 			pit1.addMouseListener(new java.awt.event.MouseAdapter() {
 				public void mouseClicked(java.awt.event.MouseEvent e) {
-					System.out.println("mouseClicked()"); // TODO Auto-generated Event stub mouseClicked()
+					System.out.println("mouseClicked()"); // TODO Auto-generated
+					// Event stub
+					// mouseClicked()
 				}
 			});
 		}
@@ -313,19 +308,20 @@ public class MancalaGUI {
 	}
 
 	/**
-	 * This method initializes pit2	
-	 * 	
-	 * @return javax.swing.JButton	
+	 * This method initializes pit2
+	 * 
+	 * @return javax.swing.JButton
 	 */
-	private JButton getPit2() {
+	JButton getPit2() {
 		if (pit2 == null) {
 			pit2 = new JButton();
-			pit2.setText("3");
 			pit2.setFont(new Font("Dialog", Font.BOLD, 14));
 			pit2.setPreferredSize(new Dimension(60, 26));
 			pit2.addMouseListener(new java.awt.event.MouseAdapter() {
 				public void mouseClicked(java.awt.event.MouseEvent e) {
-					System.out.println("mouseClicked()"); // TODO Auto-generated Event stub mouseClicked()
+					System.out.println("mouseClicked()"); // TODO Auto-generated
+					// Event stub
+					// mouseClicked()
 				}
 			});
 		}
@@ -333,19 +329,20 @@ public class MancalaGUI {
 	}
 
 	/**
-	 * This method initializes pit3	
-	 * 	
-	 * @return javax.swing.JButton	
+	 * This method initializes pit3
+	 * 
+	 * @return javax.swing.JButton
 	 */
-	private JButton getPit3() {
+	JButton getPit3() {
 		if (pit3 == null) {
 			pit3 = new JButton();
-			pit3.setText("3");
 			pit3.setFont(new Font("Dialog", Font.BOLD, 14));
 			pit3.setPreferredSize(new Dimension(60, 26));
 			pit3.addMouseListener(new java.awt.event.MouseAdapter() {
 				public void mouseClicked(java.awt.event.MouseEvent e) {
-					System.out.println("mouseClicked()"); // TODO Auto-generated Event stub mouseClicked()
+					System.out.println("mouseClicked()"); // TODO Auto-generated
+					// Event stub
+					// mouseClicked()
 				}
 			});
 		}
@@ -353,19 +350,20 @@ public class MancalaGUI {
 	}
 
 	/**
-	 * This method initializes pit4	
-	 * 	
-	 * @return javax.swing.JButton	
+	 * This method initializes pit4
+	 * 
+	 * @return javax.swing.JButton
 	 */
-	private JButton getPit4() {
+	JButton getPit4() {
 		if (pit4 == null) {
 			pit4 = new JButton();
-			pit4.setText("3");
 			pit4.setFont(new Font("Dialog", Font.BOLD, 14));
 			pit4.setPreferredSize(new Dimension(60, 26));
 			pit4.addMouseListener(new java.awt.event.MouseAdapter() {
 				public void mouseClicked(java.awt.event.MouseEvent e) {
-					System.out.println("mouseClicked()"); // TODO Auto-generated Event stub mouseClicked()
+					System.out.println("mouseClicked()"); // TODO Auto-generated
+					// Event stub
+					// mouseClicked()
 				}
 			});
 		}
@@ -373,19 +371,20 @@ public class MancalaGUI {
 	}
 
 	/**
-	 * This method initializes pit5	
-	 * 	
-	 * @return javax.swing.JButton	
+	 * This method initializes pit5
+	 * 
+	 * @return javax.swing.JButton
 	 */
-	private JButton getPit5() {
+	JButton getPit5() {
 		if (pit5 == null) {
 			pit5 = new JButton();
-			pit5.setText("3");
 			pit5.setFont(new Font("Dialog", Font.BOLD, 14));
 			pit5.setPreferredSize(new Dimension(60, 26));
 			pit5.addMouseListener(new java.awt.event.MouseAdapter() {
 				public void mouseClicked(java.awt.event.MouseEvent e) {
-					System.out.println("mouseClicked()"); // TODO Auto-generated Event stub mouseClicked()
+					System.out.println("mouseClicked()"); // TODO Auto-generated
+					// Event stub
+					// mouseClicked()
 				}
 			});
 		}
@@ -393,19 +392,20 @@ public class MancalaGUI {
 	}
 
 	/**
-	 * This method initializes pit6	
-	 * 	
-	 * @return javax.swing.JButton	
+	 * This method initializes pit6
+	 * 
+	 * @return javax.swing.JButton
 	 */
-	private JButton getPit6() {
+	JButton getPit6() {
 		if (pit6 == null) {
 			pit6 = new JButton();
-			pit6.setText("3");
 			pit6.setFont(new Font("Dialog", Font.BOLD, 14));
 			pit6.setPreferredSize(new Dimension(60, 26));
 			pit6.addMouseListener(new java.awt.event.MouseAdapter() {
 				public void mouseClicked(java.awt.event.MouseEvent e) {
-					System.out.println("mouseClicked()"); // TODO Auto-generated Event stub mouseClicked()
+					System.out.println("mouseClicked()"); // TODO Auto-generated
+					// Event stub
+					// mouseClicked()
 				}
 			});
 		}
@@ -413,39 +413,41 @@ public class MancalaGUI {
 	}
 
 	/**
-	 * This method initializes pit7	
-	 * 	
-	 * @return javax.swing.JButton	
+	 * This method initializes pit7
+	 * 
+	 * @return javax.swing.JButton
 	 */
-	private JButton getPit7() {
+	JButton getPit7() {
 		if (pit7 == null) {
 			pit7 = new JButton();
-			pit7.setText("3");
 			pit7.setFont(new Font("Dialog", Font.BOLD, 14));
 			pit7.setPreferredSize(new Dimension(60, 26));
 			pit7.addMouseListener(new java.awt.event.MouseAdapter() {
 				public void mouseClicked(java.awt.event.MouseEvent e) {
-					System.out.println("mouseClicked()"); // TODO Auto-generated Event stub mouseClicked()
+					System.out.println("mouseClicked()"); // TODO Auto-generated
+					// Event stub
+					// mouseClicked()
 				}
 			});
 		}
 		return pit7;
 	}
-	
+
 	/**
-	 * This method initializes pit8	
-	 * 	
-	 * @return javax.swing.JButton	
+	 * This method initializes pit8
+	 * 
+	 * @return javax.swing.JButton
 	 */
-	private JButton getPit8() {
+	JButton getPit8() {
 		if (pit8 == null) {
 			pit8 = new JButton();
-			pit8.setText("3");
 			pit8.setFont(new Font("Dialog", Font.BOLD, 14));
 			pit8.setPreferredSize(new Dimension(60, 26));
 			pit8.addMouseListener(new java.awt.event.MouseAdapter() {
 				public void mouseClicked(java.awt.event.MouseEvent e) {
-					System.out.println("mouseClicked()"); // TODO Auto-generated Event stub mouseClicked()
+					System.out.println("mouseClicked()"); // TODO Auto-generated
+					// Event stub
+					// mouseClicked()
 				}
 			});
 		}
@@ -453,39 +455,41 @@ public class MancalaGUI {
 	}
 
 	/**
-	 * This method initializes pit9	
-	 * 	
-	 * @return javax.swing.JButton	
+	 * This method initializes pit9
+	 * 
+	 * @return javax.swing.JButton
 	 */
-	private JButton getPit9() {
+	JButton getPit9() {
 		if (pit9 == null) {
 			pit9 = new JButton();
-			pit9.setText("3");
 			pit9.setFont(new Font("Dialog", Font.BOLD, 14));
 			pit9.setPreferredSize(new Dimension(60, 26));
 			pit9.addMouseListener(new java.awt.event.MouseAdapter() {
 				public void mouseClicked(java.awt.event.MouseEvent e) {
-					System.out.println("mouseClicked()"); // TODO Auto-generated Event stub mouseClicked()
+					System.out.println("mouseClicked()"); // TODO Auto-generated
+					// Event stub
+					// mouseClicked()
 				}
 			});
 		}
 		return pit9;
 	}
-	
+
 	/**
-	 * This method initializes pit10	
-	 * 	
-	 * @return javax.swing.JButton	
+	 * This method initializes pit10
+	 * 
+	 * @return javax.swing.JButton
 	 */
-	private JButton getPit10() {
+	JButton getPit10() {
 		if (pit10 == null) {
 			pit10 = new JButton();
-			pit10.setText("3");
 			pit10.setFont(new Font("Dialog", Font.BOLD, 14));
 			pit10.setPreferredSize(new Dimension(60, 26));
 			pit10.addMouseListener(new java.awt.event.MouseAdapter() {
 				public void mouseClicked(java.awt.event.MouseEvent e) {
-					System.out.println("mouseClicked()"); // TODO Auto-generated Event stub mouseClicked()
+					System.out.println("mouseClicked()"); // TODO Auto-generated
+					// Event stub
+					// mouseClicked()
 				}
 			});
 		}
@@ -493,9 +497,9 @@ public class MancalaGUI {
 	}
 
 	/**
-	 * This method initializes rightPanel	
-	 * 	
-	 * @return javax.swing.JPanel	
+	 * This method initializes rightPanel
+	 * 
+	 * @return javax.swing.JPanel
 	 */
 	private JPanel getRightPanel() {
 		if (rightPanel == null) {
@@ -521,9 +525,9 @@ public class MancalaGUI {
 	}
 
 	/**
-	 * This method initializes helpButton	
-	 * 	
-	 * @return javax.swing.JButton	
+	 * This method initializes helpButton
+	 * 
+	 * @return javax.swing.JButton
 	 */
 	private JButton getHelpButton() {
 		if (helpButton == null) {
@@ -546,9 +550,9 @@ public class MancalaGUI {
 	}
 
 	/**
-	 * This method initializes teamButton	
-	 * 	
-	 * @return javax.swing.JButton	
+	 * This method initializes teamButton
+	 * 
+	 * @return javax.swing.JButton
 	 */
 	private JButton getTeamButton() {
 		if (teamButton == null) {
@@ -572,9 +576,9 @@ public class MancalaGUI {
 	}
 
 	/**
-	 * This method initializes helpDialog	
-	 * 	
-	 * @return javax.swing.JDialog	
+	 * This method initializes helpDialog
+	 * 
+	 * @return javax.swing.JDialog
 	 */
 	private JDialog getHelpDialog() {
 		if (helpDialog == null) {
@@ -588,9 +592,9 @@ public class MancalaGUI {
 	}
 
 	/**
-	 * This method initializes help	
-	 * 	
-	 * @return javax.swing.JPanel	
+	 * This method initializes help
+	 * 
+	 * @return javax.swing.JPanel
 	 */
 	private JPanel getHelp() {
 		if (help == null) {
@@ -606,23 +610,24 @@ public class MancalaGUI {
 	}
 
 	/**
-	 * This method initializes helpText	
-	 * 	
-	 * @return javax.swing.JTextPane	
+	 * This method initializes helpText
+	 * 
+	 * @return javax.swing.JTextPane
 	 */
 	private JTextPane getHelpText() {
 		if (helpText == null) {
 			helpText = new JTextPane();
-			helpText.setText("... uf khjg jkhg kjhg kjhg bjhg kujhg bjhg \nkjgh kjhg jkh kjh kjhg kjhv kjhv kjhv kjhv ");
+			helpText
+					.setText("... uf khjg jkhg kjhg kjhg bjhg kujhg bjhg \nkjgh kjhg jkh kjh kjhg kjhv kjhv kjhv kjhv ");
 			helpText.setBackground(SystemColor.activeCaptionBorder);
 		}
 		return helpText;
 	}
 
 	/**
-	 * This method initializes newgameButton	
-	 * 	
-	 * @return javax.swing.JButton	
+	 * This method initializes newgameButton
+	 * 
+	 * @return javax.swing.JButton
 	 */
 	private JButton getNewgameButton() {
 		if (newgameButton == null) {
@@ -632,23 +637,13 @@ public class MancalaGUI {
 			newgameButton.setPreferredSize(new Dimension(120, 26));
 			newgameButton.addMouseListener(new java.awt.event.MouseAdapter() {
 				public void mouseClicked(java.awt.event.MouseEvent e) {
-					System.out.println("New game!"); // TODO Auto-generated Event stub mouseClicked()
+					System.out.println("New game!"); // TODO Auto-generated
+					// Event stub
+					// mouseClicked()
 				}
 			});
 		}
 		return newgameButton;
-	}
-
-	/**
-	 * Launches this application
-	 */
-	public static void main(String[] args) {
-		SwingUtilities.invokeLater(new Runnable() {
-			public void run() {
-				MancalaGUI application = new MancalaGUI();
-				application.getJFrame().setVisible(true);
-			}
-		});
 	}
 
 }
