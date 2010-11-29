522a3faeeec5558607ef38ec8e2d3917427c7e75
Hans <hans@maesalu.com>
2010-11-29 02:23:54 +0200

    Artjom makes a move from pit 8 and gets a new move

--- Mancala/generated/classesAllTests.java
+++ Mancala/generated/classesAllTests.java
@@ -26,6 +26,8 @@ public class classesAllTests
          suite = new TestSuite ( );
 
          // collabStat call
+         suite.addTestSuite (Artjom_makes_a_move_from_pit_8_and_gets_a_new_moveTest.class);
+         // collabStat call
          suite.addTestSuite (Making_a_drawTest.class);
          // collabStat call
          suite.addTestSuite (Ulno_wins_the_gameTest.class);
